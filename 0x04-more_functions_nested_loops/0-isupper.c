6a5744661bd8d254c2543a2905d7d4a762140bfe
zubeyda <nurzubeyd@gmail.com>
2022-06-16 12:23:21 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,26 +1,17 @@
-#include "holberton.h"
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code for Holberton School students.
- * c should start from 65 upto 90 for capital letter the rest should be declined
- * _isupper - Check if paramter c represents an uppercase letter.
- * @c: Int representing a character
- * Return: Always 0.
- */
+*_isupper - checks if a  character is uppercase or not
+*@c: character to be tested
+*Return: 1 whether it is, 0 otherwise
+*/
 
-int _isupper(int c){
-    if(c >= 65 && c <=90){
-        return (1);
-    }else{
-        return (0);
-    }
-    return 0;
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
+
 	return (0);
 }
