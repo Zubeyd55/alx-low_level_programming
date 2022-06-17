dc51c190e6a03df67d1cfdf3f1774b3e96603816
zubeyda <nurzubeyd@gmail.com>
2022-06-17 13:39:40 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,12 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-/**
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
- */
+#ifndef _main_h_
+#define _main_h_
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -17,9 +10,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-char *rot13(char *);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
 
 #endif
